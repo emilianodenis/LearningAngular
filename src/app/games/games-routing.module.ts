f9b0bd365a44c0d6b226112179ec4bcd7dd4b255
emilianodenis <emiliano.denis@gmail.com>
2021-04-27 15:35:13 -0400

    fix relative path problem

--- src/app/games/games-routing.module.ts
+++ src/app/games/games-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { GameComponent } from 'src/app/games/game/game.component';
 import { HomeComponent } from 'src/app/games/home/home.component';
 import { MinesweeperComponent } from 'src/app/games/minesweeper/minesweeper.component';
 import { TaquinComponent } from 'src/app/games/taquin/taquin.component';
@@ -9,10 +8,6 @@ const routes: Routes = [
     {
         path: "",
         component: HomeComponent,
-    },
-    {
-        path: "games",
-        component: GameComponent,
         children: [
             {
                 path: "minesweeper",
