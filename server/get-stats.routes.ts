b12dc65b655f5ab01630dc956474f358dbe579ef
emilianodenis <emiliano.denis@gmail.com>
2021-04-29 22:21:45 -0400

    small refacto UI

--- server/get-stats.routes.ts
+++ server/get-stats.routes.ts
@@ -6,7 +6,7 @@ import { STATS } from './data';
 export function getAllStats(req: Request, res: Response) {
     setTimeout(() => {
 
-        res.status(200).json({ payload: Object.values(STATS) });
+        res.status(200).json({ payload: Object.values([...STATS,...STATS,...STATS,...STATS,...STATS,...STATS,...STATS,...STATS]) });
 
     }, 400);
 }
